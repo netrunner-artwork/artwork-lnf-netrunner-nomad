b2bc3f0a4493e262789041a7514f3d57eee9e609
Rohan Garg <rohan@garg.io>
2017-10-04 13:15:28 +0200

    Add custom panel at the bottom customization for nomadmenu

---
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-nomad-desktop.desktop/contents/layouts/org.kde.plasma.desktop.defaultPanel/contents/layout.js
@@ -0,0 +1,25 @@
+var plasma = getApiVersion(1)
+
+// Create top pannel
+panel = new plasma.Panel
+panel.location = "bottom"
+panel.alignment = "center"
+
+// Add widgets
+var menu = panel.addWidget("org.kde.plasma.nomadmenu")
+
+panel.addWidget("org.nomad-shell.panel.separator")
+
+panel.addWidget("org.kde.plasma.appmenu")
+panel.addWidget("org.kde.plasma.panelspacer")
+var systray = panel.addWidget("org.nomad.systemtray")
+
+// Don't load widgets overwriten in nomad
+var systrayContainmentId = systray.readConfig('SystrayContainmentId')
+var systrayContainer = desktopById(systrayContainmentId)
+systrayContainer.currentConfigGroup = ['General']
+systrayContainer.writeConfig("knownItems", "org.kde.plasma.networkmanagement,org.kde.plasma.notifications,org.kde.plasma.mediacontroller,org.kde.plasma.devicenotifier,org.kde.plasma.volume")
+
+panel.addWidget("org.nomad.clock")
+
+panel.locked = true
